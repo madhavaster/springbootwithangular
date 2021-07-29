304c6dc8e74546443ea02a7827cac786629418e7
Madhava Reddy <madhava@agrisoft.com>
2021-07-29 15:12:13 +0530

    initial commit

---
+++ src/main/java/com/agrisutra/SpringBootWithAngularApplication.java
@@ -0,0 +1,18 @@
+package com.agrisutra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithAngularApplication.class, args);
+	}
+
+}
